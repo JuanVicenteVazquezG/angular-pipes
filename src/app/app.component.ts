9d8793f29a95d5a0fc6e647a6186524df9c2ea76
Juan Vicente Vazquez <juanvicentevazquezg@gmail.com>
2021-01-08 09:48:34 +0100

    pipe json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,15 @@ export class AppComponent {
   Array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   PI: number = Math.PI;
   percentage = 0.234;
+  salary = 1234.5;
+
+  hero = {
+    name: 'Logan',
+    key: 'Wolverine',
+    age: 500,
+    address: {
+      street: 'First',
+      house: 20,
+    },
+  };
 }
